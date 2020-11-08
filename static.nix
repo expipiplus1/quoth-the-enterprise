5cf1e84f0f6181ae4991eccd546b0f0c2de27ca9
Joe Hermaszewski <git@monoid.al>
2020-11-09 00:44:06 +0800

    Use nixpkgs with several fixes for musl ghc

--- static.nix
+++ static.nix
@@ -3,8 +3,8 @@
     "https://github.com/expipiplus1/nixpkgs/archive/d732d4cee0c276f48b4de96ed69ccd6a660e9198.tar.gz"; # joe-haskell-cross
   sha256 = "1dbizm8s6kr05r8g5in20h9ncc4fcwqvdph8bdgkgcr8a1g6y273";
 }, crossSystem ? null
-, pkgs ? (import nixpkgsSrc { inherit crossSystem; }).pkgsMusl, compiler ? null
-}:
+, pkgs ? (import nixpkgsSrc { inherit crossSystem; }).pkgsStatic
+, compiler ? null }:
 
 let
   extraOverrides = with pkgs.haskell.lib;
